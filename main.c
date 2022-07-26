cc12f0aba8eb200abcbc56a058ec332f1e8c6853
Jocelyn FOUCHER <jfoucher@e1r3p15.clusters.42paris.fr>
2022-07-26 05:57:28 +0200

    checking for syntax error with redirections

--- main.c
+++ main.c
@@ -68,6 +68,29 @@ int	check_for_unexpected_token(t_list *token_list)
 		g_status = 2;
 		return (-1);
 	}
+	while (token_list && token_list->next)
+	{
+		if (get_token(token_list)->type != WORD
+			&& get_token(token_list)->type != PIPE)
+		{
+			if (get_token(token_list->next)->type != WORD)
+			{
+				ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+				ft_putstr_fd(get_token(token_list->next)->value->buffer, 2);
+				ft_putstr_fd("'\n", 2);
+				g_status = 2;
+				return (-1);
+			}
+		}
+		token_list = token_list->next;
+	}
+	if (get_token(token_list)->type != WORD
+		&& get_token(token_list)->type != PIPE)
+	{
+		ft_putstr_fd("minishell: syntax error near unexpected token `newline'\n", 2);
+		g_status = 2;
+		return (-1);
+	}
 	return (0);
 }
 
